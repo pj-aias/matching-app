abb5aab65825ebbf38fac2d10a51bceac3d285a8
kino-ma <kino.ma.ms@gmail.com>
2021-08-29 23:47:02 +0900

    [feat] show user and content of messages

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, Button } from 'react-native';
+import { sendAPIRequestAuth } from '../util/api';
 
-const Chat = () => {
+const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
+
+  const { roomId } = route.params;
+
+  useEffect(() => {
+    sendAPIRequestAuth('/message/' + roomId, {
+      method: 'GET'
+    }).then((res) => {
+      console.log('GET /message');
+      console.log(res);
+      setMessages(res.data.messages);
+    })
+  }, [roomId]);
+
+  const messagesView = messages.map((m) => <Message content={m.content} user={m.user} />)
+  console.log(messages)
+
   return (
     <View>
       <Text>Chat</Text>
-      <Button title="Go to Details" onPress={() => {}} />
+      {messagesView}
+      <Button title="Go to Details" onPress={() => { }} />
     </View>
   );
-}
+};
+
+const Message = ({ content, user }) => (
+  <Text>{user.username}: {content}</Text>
+);
 
 export default Chat;
