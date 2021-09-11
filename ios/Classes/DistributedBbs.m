6837eabe357ea77cb70d100218d128b53f0247ac
kino-ma <kino.ma.ms@gmail.com>
2021-09-11 14:08:56 +0900

    [refactor] re-organize objc / swift source files

---
+++ ios/Classes/DistributedBbs.m
@@ -0,0 +1,14 @@
+//
+//  DistributedBbs.m
+//  MatchingApp
+//
+//  Created by 牧野青希 on 2021/08/31.
+//
+
+#import "DistributedBbsModule.h"
+
+
+@interface DistributedBbsModule
+RCT_EXPORT_MODULE(DistributedBbs);
+@end
+
