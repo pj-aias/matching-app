851f055356c9bfc0c475c5da843e3f2cce755341
kino-ma <kino.ma.ms@gmail.com>
2021-09-23 00:40:30 +0900

    [feat] connect API server locally if it is debug state

--- util/api.js
+++ util/api.js
@@ -1,5 +1,48 @@
 import axios from 'axios';
+import { EventEmitter } from 'react-native';
 import Tor from 'react-native-tor';
+
+class DirectAPI {
+    constructor() { }
+
+    startIfNotStarted() { }
+
+    async get(url, headers, trustSSL) {
+        let resp = await axios({
+            method: 'GET',
+            url,
+            headers,
+        });
+
+        resp.json = resp.data
+
+        return resp
+    }
+
+    async post(url, body, headers, trustSSL) {
+        let resp = await axios({
+            method: 'POST',
+            url,
+            headers,
+            data: body,
+        })
+        resp.json = resp.data;
+
+        return resp;
+    }
+
+    async delete(url, body, headers, trustSSL) {
+        let resp = await axios({
+            method: 'DELETE',
+            url,
+            headers,
+            data: body,
+        });
+
+        resp.json = resp.data;
+        return resp;
+    }
+}
 
 export class APIHandler {
     static tor = Tor();
@@ -9,15 +52,26 @@ export class APIHandler {
     static host = 'yjqictkblqijcsldpwvkd2addy2kc7edpffeg64lhynruy3kxl7s5zid.onion';
     static port = 80;
 
+    static me = null;
+
     static buildApiUrl(path, host = APIHandler.host) {
-        return `${APIHandler.scheme}://${host}:${APIHandler.port}${path}`;
+        const url = `${APIHandler.scheme}://${host}:${APIHandler.port}${path}`;
+        console.log({ url });
+        return url;
     }
 
-    static setAuthToken(tokenString) {
+    static setAuthInfo(user, tokenString) {
+        APIHandler.me = user;
         APIHandler.authToken = tokenString;
     }
 
     constructor(endpoint) {
+        if (env.get('AIAS_DEBUG') === 'true') {
+            APIHandler.tor = new DirectAPI();
+            APIHandler.host = env.get('AIAS_API_HOST')
+            APIHandler.port = env.get('AIAS_API_PORT')
+        }
+
         APIHandler.tor.startIfNotStarted();
         this.authToken = '';
         this.url = APIHandler.buildApiUrl(endpoint);
@@ -58,4 +112,8 @@ export class APIHandler {
         console.log(`DELETE ${this.endpoint}`);
         return APIHandler.tor.delete(this.url, body, headers);
     }
+}
+
+export const useCurrentUser = () => {
+    return APIHandler.me;
 }
