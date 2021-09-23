87b1963e231c5222123b2c8ebc368f0186f03948
akakou <akakou571@gmail.com>
2021-09-23 15:00:48 +0900

    [add] Get data from aias app

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import 'react-native-gesture-handler';
-import React, {useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 import Match from './components/screens/Matching/Match';
 import Top from './components/screens/Auth/Top';
 import Signin from './components/screens/Auth/SignIn';
 import Signup from './components/screens/Auth/Signup';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Chat from './components/screens/Chat';
 import Start from './components/screens/Matching/Start';
 import ChatIndex from './components/screens/ChatIndex';
 import TestTorScreen from './components/screens/TestTorScreen';
-import {APIHandler} from './util/api';
+import { APIHandler } from './util/api';
 
 const theme = {
   headerStyle: {
@@ -25,11 +25,34 @@ const theme = {
   },
 };
 
+const Stack = createStackNavigator();
+
+const config = {
+  screens: {
+    TOP: {
+      path: 'result',
+      parse: {
+        result: (data) => `?result=${data}`,
+      },
+      stringify: {
+        result: (data) => {
+          return data.replace(/^\?result=/, '');
+        },
+      },
+    },
+  },
+};
+
+const linking = {
+  prefixes: ['https://anomatch.com', 'anomatch://'],
+  config,
+};
+
 const App = () => {
   // prefetch to establish TCP connection to API server
-  useEffect(() => {
-    new APIHandler('/').get().then(console.log);
-  }, []);
+  // useEffect(() => {
+  //   new APIHandler('/').get().then(console.log);
+  // }, []);
 
   return (
     <SafeAreaProvider>
