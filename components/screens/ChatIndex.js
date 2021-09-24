ee85a73789c798d1992e337bf1451b6f08a5138e
kino-ma <kino.ma.ms@gmail.com>
2021-09-24 18:02:07 +0900

    [fix] replace placeholder text to real username

--- components/screens/ChatIndex.js
+++ components/screens/ChatIndex.js
@@ -1,11 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import {Text, View, Button, TouchableOpacity} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
-import {APIHandler} from '../../util/api';
-import {getUserNames} from './Chat.js';
+import React, { useEffect, useState } from 'react';
+import { Text, View, Button, TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { APIHandler } from '../../util/api';
+import { getUserNames } from './Chat.js';
 
-const ChatIndex = ({navigation}) => {
+const ChatIndex = ({ navigation }) => {
   const [rooms, setRooms] = useState([]);
+  const [otherUser, setOtherUser] = useState(null);
+
+  const me = APIHandler.whoami();
 
   // Get messages from API after render (effect), and store them to variable if succeeded
   useEffect(() => {
@@ -20,6 +23,9 @@ const ChatIndex = ({navigation}) => {
         console.log('get chat index');
         console.log(res);
         setRooms(res.json.chatrooms);
+
+        const user = res.json.chatroom.users.filter((u) => u.id !== me.id)[0];
+        setOtherUser(other)
       })
       .catch(console.log);
   }, []);
@@ -40,7 +46,7 @@ const ChatIndex = ({navigation}) => {
   return <ScrollView>{roomsView}</ScrollView>;
 };
 
-const Room = ({room, openRoom}) => {
+const Room = ({ room, openRoom }) => {
   const usernames = getUserNames(room);
 
   return (
@@ -72,7 +78,7 @@ const Room = ({room, openRoom}) => {
           fontSize: 19,
           fontWeight: 'bold',
         }}>
-        Mika
+        {otherUser.username}
       </Text>
     </TouchableOpacity>
   );
